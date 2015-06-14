38d61b8877e7c0917bbce509240d59fec653c59a
Matt Ward <ward.matt@gmail.com>
2015-06-14 19:13:50 +0100

    Allow .xproj file to be loaded.
    .xproj files can now be opened in SharpDevelop.
    
    Added a workaround to allow .xproj files to be opened when the DNX
    .targets and .props files are not installed. Workaround is to define
    VSToolsPath as an empty string in the global MSBuild properties when
    loading the project. Otherwise an error will be displayed about the
    Microsoft.DNX.Props file not being found.

---
+++ src/AddIns/BackendBindings/AspNet/Project/Src/DnxMSBuildTargetsDirectory.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) 2015 AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.IO;
+
+namespace ICSharpCode.AspNet
+{
+	public class DnxMSBuildTargetsDirectory
+	{
+		static readonly string[] msbuildTargetFileNames = {
+			"Microsoft.DNX.Props",
+			"Microsoft.DNX.targets"
+		};
+		
+		public bool TargetFilesExist()
+		{
+			string directory = GetDirectory();
+			if (!Directory.Exists(directory))
+				return false;
+			
+			return FilesExist(directory);
+		}
+		
+		public string GetDirectory()
+		{
+			return Path.Combine(
+				GetMSBuildExtensionsPath(),
+				"Microsoft",
+				"VisualStudio",
+				"v14",
+				"DNX");
+		}
+		
+		string GetMSBuildExtensionsPath()
+		{
+			return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86), "MSBuild");
+		}
+		
+		bool FilesExist(string directory)
+		{
+			foreach (string fileName in msbuildTargetFileNames) {
+				string fullFileNamePath = Path.Combine(directory, fileName);
+				if (!File.Exists(fullFileNamePath))
+					return false;
+			}
+			
+			return true;
+		}
+	}
+}
