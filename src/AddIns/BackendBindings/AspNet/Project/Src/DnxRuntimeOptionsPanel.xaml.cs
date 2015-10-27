fb9f389d0d984429ad77b9f1284e381ce97c960e
Matt Ward <ward.matt@gmail.com>
2015-10-27 20:20:07 +0000

    DNX runtime version can be changed in project options.
    Added a project options panel: DNX Runtime which allows the
    DNX SDK version number to be changed in the global.json file.
    
    This is a project options panel and not a solution options panel so
    option can be hidden in the dialog if the project is not a DNX
    project.
    
    The list of DNX runtime versions is currently hard coded. The
    installed versions should be determined and listed instead.
    
    The format of the global.json file is currently fixed to use tabs.
    It should use a custom code formatter to allow the formatting to be
    customised.

---
+++ src/AddIns/BackendBindings/AspNet/Project/Src/DnxRuntimeOptionsPanel.xaml.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) 2015 AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ICSharpCode.SharpDevelop.Gui.OptionPanels;
+using ICSharpCode.SharpDevelop.Project;
+
+namespace ICSharpCode.AspNet
+{
+	public partial class DnxRuntimeOptionsPanel : ProjectOptionPanel
+	{
+		GlobalJsonFile globalJsonFile;
+		List<string> dnxRuntimeVersions;
+		string originalDnxRuntimeVersion;
+		string selectedDnxRuntimeVersion;
+		
+		public DnxRuntimeOptionsPanel()
+		{
+			InitializeComponent();
+			DataContext = this;
+		}
+		
+		protected override void Load(MSBuildBasedProject project, string configuration, string platform)
+		{
+			dnxRuntimeVersions = GetDnxRuntimeVersions().ToList();
+			globalJsonFile = GlobalJsonFile.Read((AspNetProject)project);
+			if (globalJsonFile.Exists) {
+				originalDnxRuntimeVersion = globalJsonFile.DnxRuntimeVersion;
+			}
+			 
+			if (globalJsonFile == null)
+				return;
+			
+			if (globalJsonFile.Exists) {
+				SelectDnxRuntimeVersionInComboBox(globalJsonFile.DnxRuntimeVersion);
+			}
+			
+			RaisePropertyChanged(null);
+		}
+
+		IEnumerable<string> GetDnxRuntimeVersions()
+		{
+			return new string[] {
+				"1.0.0-beta4",
+				"1.0.0-beta5",
+				"1.0.0-beta6",
+				"1.0.0-beta7",
+				"1.0.0-beta8"
+			};
+		}
+		
+		public IEnumerable<string> DnxRuntimeVersions {
+			get { return dnxRuntimeVersions; }
+		}
+		
+		public string SelectedDnxRuntimeVersion {
+			get { return selectedDnxRuntimeVersion; }
+			set {
+				selectedDnxRuntimeVersion = value;
+				IsDirty = (originalDnxRuntimeVersion != selectedDnxRuntimeVersion);
+			}
+		}
+		
+		protected override bool Save(MSBuildBasedProject project, string configuration, string platform)
+		{
+			if (globalJsonFile == null)
+				return true;
+		
+			globalJsonFile.DnxRuntimeVersion = SelectedDnxRuntimeVersion;
+			if (originalDnxRuntimeVersion != globalJsonFile.DnxRuntimeVersion) {
+				globalJsonFile.Save ();
+			}
+			
+			IsDirty = false;
+			
+			return true;
+		}
+		
+		void SelectDnxRuntimeVersionInComboBox(string version)
+		{
+			selectedDnxRuntimeVersion = dnxRuntimeVersions
+				.FirstOrDefault(dnxRuntimeVersion => dnxRuntimeVersion == version);
+		}
+	}
+}
---
+++ src/AddIns/BackendBindings/AspNet/Project/Src/GlobalJsonFile.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) 2015 AlphaSierraPapa for the SharpDevelop Team
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.IO;
+using System.Text;
+using ICSharpCode.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ICSharpCode.AspNet
+{
+	public class GlobalJsonFile
+	{
+		readonly FileName filePath;
+		Encoding encoding = Encoding.UTF8;
+		JObject jsonObject;
+		JObject sdkObject;
+
+		GlobalJsonFile(FileName filePath)
+		{
+			this.filePath = filePath;
+		}
+
+		public static GlobalJsonFile Read(AspNetProject project)
+		{
+			var jsonFile = new GlobalJsonFile(project.ParentSolution.Directory.CombineFile("global.json"));
+			jsonFile.Read ();
+			return jsonFile;
+		}
+
+		public bool Exists { get; private set; }
+
+		public string DnxRuntimeVersion { get; set; }
+
+		void Read()
+		{
+			Exists = JsonFileExists();
+			if (!Exists)
+				return;
+
+			using (var fileStream = File.OpenRead(filePath)) {
+				using (var reader = new StreamReader(fileStream)) {
+					using (var jsonReader = new JsonTextReader(reader)) {
+						jsonObject = JObject.Load(jsonReader);
+						if (reader.CurrentEncoding != null) {
+							encoding = reader.CurrentEncoding;
+						}
+					}
+				}
+			}
+
+			ReadPropertiesFromJsonObject();
+		}
+
+		bool JsonFileExists()
+		{
+			return File.Exists(filePath);
+		}
+
+		void ReadPropertiesFromJsonObject ()
+		{
+			JToken sdkToken;
+			if (!jsonObject.TryGetValue("sdk", out sdkToken))
+				return;
+
+			sdkObject = sdkToken as JObject;
+			if (sdkObject == null)
+				return;
+
+			JToken version;
+			if (!sdkObject.TryGetValue("version", out version))
+				return;
+
+			DnxRuntimeVersion = version.ToString();
+		}
+
+		public void Save()
+		{
+			sdkObject["version"] = new JValue(DnxRuntimeVersion);
+			using (var writer = new StreamWriter(filePath, false, encoding)) {
+				using (var jsonWriter = new JsonTextWriter(writer)) {
+					jsonWriter.Formatting = Formatting.Indented;
+					jsonWriter.Indentation = 1;
+					jsonWriter.IndentChar = '\t';
+					jsonObject.WriteTo (jsonWriter);
+				}
+			}
+		}
+
+		public FileName Path {
+			get { return filePath; }
+		}
+	}
+}
